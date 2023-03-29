1b87392940725d14014c0287379320a8f7187c98
lex <lex@users.noreply.github.com>
2023-03-29 19:43:57 +0300

    take the rom from args

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ mod controller;
 mod memory;
 mod rom;
 
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::process;
+
 use cpu::CPU;
 use ppu::PPU;
 use apu::APU;
@@ -13,8 +18,21 @@ use memory::Memory;
 use rom::Rom;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <path/to/rom/file.nes>", args[0]);
+        process::exit(1);
+    }
+
+    let rom_path = &args[1];
     let mut memory = Memory::new();
-    let rom = Rom::load_from_file("path/to/rom/file.nes").unwrap(); // Handle errors appropriately
+    let rom = match Rom::load_from_file(rom_path) {
+        Ok(rom) => rom,
+        Err(e) => {
+            eprintln!("Error loading ROM: {}", e);
+            process::exit(1);
+        }
+    };
     memory.load_rom(&rom);
 
 
